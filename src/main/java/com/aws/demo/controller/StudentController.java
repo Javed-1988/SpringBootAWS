4aa310c83e475a30aa9c0ed27c4c152c15a5a22f
anikatahsin17 <anikatahsin17@gmail.com>
2024-12-26 18:07:50 +0530

    first commit to spring boot AWS

---
+++ src/main/java/com/aws/demo/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.aws.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class StudentController {
+
+
+    @GetMapping("/")
+    public String helloAws()
+    {
+        return "Welcome to Springboot with AWS";
+    }
+}

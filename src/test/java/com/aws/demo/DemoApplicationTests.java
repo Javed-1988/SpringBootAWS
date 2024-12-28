45530cbf3b1ee692fc2d9b4d92480b5b2285d2f3
anikatahsin17 <anikatahsin17@gmail.com>
2024-12-29 00:55:58 +0530

    commiting

--- src/test/java/com/aws/demo/DemoApplicationTests.java
+++ src/test/java/com/aws/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.aws.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void test()
+	{
+		Assertions.assertEquals(5,5);
 	}
 
 }
